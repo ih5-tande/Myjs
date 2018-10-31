44ea1bed2dc940a652fac615ba38d3cc8e85f2d1
ih5-tande <511508730@qq.com>
2018-10-31 11:49:39 +0800

    Initial commit

---
+++ showPic.js
@@ -0,0 +1,7 @@
+function showPic (whichPic){
+    var source=whichPic.getAttribute("href")
+    var placeHolder=document.getElementById("placeloader");
+    placeHolder.setAttribute("src",source);
+    var body_element=document.getElementsByTagName("body")[0];
+    console.log(body_element.childNodes);
+}
