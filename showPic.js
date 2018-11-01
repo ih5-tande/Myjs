7a8989c793775ef877d1b7037611c4bcc08139d5
ih5-tande <511508730@qq.com>
2018-11-01 18:49:08 +0800

    v11.1.18：35

--- showPic.js
+++ showPic.js
@@ -1,7 +1,8 @@
-function showPic (whichPic){
-    var source=whichPic.getAttribute("href")
-    var placeHolder=document.getElementById("placeloader");
-    placeHolder.setAttribute("src",source);
-    var body_element=document.getElementsByTagName("body")[0];
+function showPic (whichPic)
+{
+    var source = whichPic.getAttribute("href");
+    var placeHolder = document.getElementById("placeloader");
+    placeHolder.setAttribute("src", source);
+    var body_element = document.getElementsByTagName("body")[0];
     console.log(body_element.childNodes);
 }
