5de1476ade288661a2a4d68543c2142469cba56e
ih5-tande <511508730@qq.com>
2018-11-02 16:17:53 +0800

    v11.2

--- showPic.js
+++ showPic.js
@@ -3,7 +3,14 @@ function showPic (whichPic)
     var source = whichPic.getAttribute("href");
     var placeHolder = document.getElementById("placeloader");
     placeHolder.setAttribute("src", source);
-    var body_element = document.getElementsByTagName("body")[0];
+
+};
+
+function countBodyChildren(){
+    var body_element=document.getElementsByTagName("body")[0];
+    /alert(body_element.nodeType);/
+    console.log(body_element.childNodes[7].nodeType);
     console.log(body_element.childNodes);
-    console.log(1849);
+
 }
+window.onload=countBodyChildren;
