393fe5c2100dd80023cf45f8ad07adae821d99f6
ih5-tande <511508730@qq.com>
2018-11-02 18:51:56 +0800

    v11.2 18:51  choose an image

--- showPic.js
+++ showPic.js
@@ -1,16 +1,18 @@
-function showPic (whichPic)
+function showPicText(whichPic)
 {
     var source = whichPic.getAttribute("href");
     var placeHolder = document.getElementById("placeloader");
     placeHolder.setAttribute("src", source);
-
+    var description=document.getElementById("description");
+    var text=whichPic.getAttribute("title");
+    console.log(description.childNodes[0].nodeValue);
 };
 
 function countBodyChildren(){
     var body_element=document.getElementsByTagName("body")[0];
-    /alert(body_element.nodeType);/
-    console.log(body_element.childNodes[7].nodeType);
-    console.log(body_element.childNodes);
+    /alert(body_element.nodeType)/;
+
+    /console.log(body_element.childNodes)/;
 
 }
-window.onload=countBodyChildren;
+/window.onload=countBodyChildren;/
